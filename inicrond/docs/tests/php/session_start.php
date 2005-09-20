df8c089df353cb943a7fb571c39e1be32df57241
Sébastien Boisvert <sebhtml@yahoo.ca>
2005-09-20 15:40:13 +0000

    Some files have been removed

--- inicrond/docs/tests/php/session_start.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_Start();
-
-$_SESSION = null;
-
-
-?>
--- inicrond/docs/tests/php/smarty.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "../../../src/includes/class/Smarty-2.6.6/libs/Smarty.class.php";
-
-
-$smarty = new Smarty;
-
-?>
