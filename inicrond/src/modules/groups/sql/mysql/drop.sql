0bfed49df2636896328ea6a6bd5b210196de1aec
Sébastien Boisvert <sebhtml@yahoo.ca>
2005-09-20 23:49:18 +0000

    *** empty log message ***

--- inicrond/src/modules/groups/sql/mysql/drop.sql
+++
@@ -1,5 +0,0 @@
-# $Id$
-
-DROP TABLE IF EXISTS sebhtml_groups ;
-
-DROP TABLE IF EXISTS sebhtml_groups_usrs ;
--- inicrond/src/modules/members/sql/mysql/insert.sql
+++
@@ -1,8 +0,0 @@
-# $Id$
-
-INSERT INTO 
-sebhtml_usrs 
-(usr_name, usr_md5_password) 
-VALUES 
-('nobody', '');
-
