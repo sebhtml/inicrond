b997e41ae86e685e2018fca3010307246bf5bd2b
Sébastien Boisvert <sebhtml@yahoo.ca>
2005-10-29 00:36:47 +0000

    The bug labelled #1330049 on sourceforge have been fixed this bug was that the courses were listed even if the user did not send a cookie to the server causing a php fatal error.

--- inicrond/src/modules/courses/courses.php
+++ inicrond/src/modules/courses/courses.php
@@ -1,21 +1,16 @@
 <?php
-//$Id$
-/*
-//---------------------------------------------------------------------
-//
-//
-//Fonction du fichier : l'index du site
-//
-//
+/* $Id$ */
+
+/**********************************************************************************
+
 //Auteur : sebastien boisvert
 //email : sebhtml@users.sourceforge.net
 //site web : http://inicrond.sourceforge.net/
 //Projet : inicrond
 
-Copyright (C) 2004  Sebastien Boisvert
+Copyright (C) 2004-2005  Sebastien Boisvert
 
-http://www.gnu.org/copyleft/gpl.html
-
+/*
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
@@ -30,112 +25,81 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-//
-//---------------------------------------------------------------------
-*/
+**********************************************************************************/
+
 define ('__INICROND_INCLUDED__', TRUE);
 define ('__INICROND_INCLUDE_PATH__', '../../');
+
 include __INICROND_INCLUDE_PATH__.'includes/kernel/pre_modulation.php';
 include 'includes/languages/'.$_SESSION['language'].'/lang.php';
 
-
-
-
-//-----------------------
-// titre
-//---------------------
+if (!isset($_SESSION['usr_id']))// you have to be loggued to see this page
+{
+    exit();
+}
 
 $module_title = $_LANG['courses'];
 
-if ($_SESSION['SUID'])		//admin can add a course
-  {
-    $module_content .=  "<br />";
-    $module_content .= 
-      retournerHref ("../../modules/courses/add_edit_course.php",
-		     $_LANG['add_course']);
-    $module_content .=  "<br />";
+if ($_SESSION['SUID']) //admin can add a course
+{
+    $module_content .= '<br />';
+    $module_content .= retournerHref ('../../modules/courses/add_edit_course.php', $_LANG['add_course']);
+    $module_content .= '<br />';
 
-  }
+    $query = '  SELECT 
+	        cours_id,
+                cours_name,
+                cours_code
+                FROM
+	'.$_OPTIONS['table_prefix'].$_OPTIONS['tables']['cours'].' AS t1
+	';
 
+}
 //get all the course with only one query!!!!!!!!!!.
-if (!$_SESSION['SUID'])		//not suid
-  {
-    $query = "SELECT 
-	
+else		//not suid
+{
+    $query = 'SELECT 
 	DISTINCT 
 	t1.cours_id AS cours_id,
         cours_name,
         cours_code
-        
-        
 	FROM
-	".$_OPTIONS['table_prefix'].$_OPTIONS['tables']['cours']." AS t1,
-	".$_OPTIONS['table_prefix'].$_OPTIONS['tables']['groups_usrs']." AS t2,
-	".$_OPTIONS['table_prefix'].$_OPTIONS['tables']['groups']." AS t3
+	'.$_OPTIONS['table_prefix'].$_OPTIONS['tables']['cours'].' AS t1,
+	'.$_OPTIONS['table_prefix'].$_OPTIONS['tables']['groups_usrs'].' AS t2,
+	'.$_OPTIONS['table_prefix'].$_OPTIONS['tables']['groups'].' AS t3
 	WHERE
-	usr_id = ".$_SESSION['usr_id']."
+	usr_id = '.$_SESSION['usr_id'].'
 	AND
 	t3.cours_id = t1.cours_id
 	AND
 	t3.group_id = t2.group_id
 	AND
-	(is_teacher_group = '1' OR is_student_group = '1')
-	";
-    //echo $query;
-
-  }
-else
-  {
-    $query = "SELECT 
-	
-        cours_id,
-        cours_name,
-        cours_code
-        
-        
-	FROM
-	".$_OPTIONS['table_prefix'].$_OPTIONS['tables']['cours']." AS t1
-	";
-
-
-  }
+	(is_teacher_group = \'1\' OR is_student_group = \'1\')
+	';
+}
 
 $rs = $inicrond_db->Execute ($query);
 
-
 $courses = array (array ($_LANG['cours_code'], $_LANG['cours_name']));
 
-
 while ($fetch_result = $rs->FetchRow ())
-  {
+{
     $course = array ();
 
     $course['cours_code'] = $fetch_result['cours_code'];
     $course['cours_name'] =
-      retournerHref (__INICROND_INCLUDE_PATH__.
-		     "modules/courses/inode.php?cours_id=".
+      retournerHref (__INICROND_INCLUDE_PATH__.'modules/courses/inode.php?cours_id='.
 		     $fetch_result['cours_id']."",
 		     $fetch_result['cours_name']);
-
-
+    
     $courses[] = $course;
 
 
-  }
-
-
-
-
-
-
+}
 
 $smarty->assign ('courses', $courses);
 
-//echo nl2br(print_r($courses, TRUE));
-
 $module_content .=  $smarty->fetch ($_OPTIONS['theme']."/courses_splash.tpl");
-
-
 
 include __INICROND_INCLUDE_PATH__.'includes/kernel/post_modulation.php';
 
