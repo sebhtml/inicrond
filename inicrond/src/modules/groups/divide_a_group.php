390e20c9e8db89e012837033c09163da942b6260
Sébastien Boisvert <sebhtml@yahoo.ca>
2005-11-17 01:45:13 +0000

    Updated MySQL dependency to >=4.1

--- inicrond/src/modules/groups/divide_a_group.php
+++ inicrond/src/modules/groups/divide_a_group.php
@@ -21,6 +21,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 -----------------------------------------------------------------------*/
+
+
 define("__INICROND_INCLUDED__", TRUE);//security
 define("__INICROND_INCLUDE_PATH__", "../../");//path
 include __INICROND_INCLUDE_PATH__."includes/kernel/pre_modulation.php";//init inicrond kernel
