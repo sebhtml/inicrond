508dfc8872ea9e9f7f8a35195fb581dd36c39465
Sébastien Boisvert <sebhtml@yahoo.ca>
2006-01-06 19:28:00 +0000

    Added some infos in documentation/

--- inicrond/patchs/3.3.1_to_3.3.2.php
+++ inicrond/patchs/3.3.1_to_3.3.2.php
@@ -399,4 +399,67 @@ CREATE TABLE ".$_OPTIONS["table_prefix"]."thread_subscription (
 
 $inicrond_db->Execute($query);
 
+$query = "
+create table ".$_OPTIONS["table_prefix"]."java_identifications_on_a_figure_result_association
+(
+    java_identifications_on_a_figure_result_id int unsigned not null,
+    java_identification_on_a_figure_label_id_source int unsigned not null,
+    java_identification_on_a_figure_label_id_destination int unsigned not null,
+    order_id int unsigned not null,
+    key java_identifications_on_a_figure_result_id (java_identifications_on_a_figure_result_id),
+    key java_identification_on_a_figure_label_id_source (java_identification_on_a_figure_label_id_source),
+    key java_identification_on_a_figure_label_id_destination (java_identification_on_a_figure_label_id_destination)
+) ;
+" ;
+
+$inicrond_db->Execute($query);
+
+$query = "
+create table ".$_OPTIONS["table_prefix"]."java_identification_on_a_figure_label
+(
+    java_identification_on_a_figure_label_id int unsigned NOT NULL auto_increment,
+    inode_id int unsigned not null,
+    label_name varchar(30) not null,
+    x_position smallint unsigned not null,
+    y_position smallint unsigned not null,
+    order_id int unsigned not null,
+    primary key (java_identification_on_a_figure_label_id),
+    key inode_id (inode_id)
+) ;
+" ;
+
+$inicrond_db->Execute($query);
+
+$query = "
+create table ".$_OPTIONS["table_prefix"]."java_identifications_on_a_figure
+(
+    inode_id int unsigned not null,
+    title varchar(30) not null,
+    at_random char(1) not null,
+    available_sheet char(1) not null,
+    available_result char(1) not null,
+    image_file_name char(32) not null,
+    add_time_t int unsigned not null,
+    edit_time_t int unsigned not null,
+    primary key (inode_id)
+) ;
+" ;
+
+$inicrond_db->Execute($query);
+
+$query = "
+create table ".$_OPTIONS["table_prefix"]."java_identifications_on_a_figure_result
+(
+    inode_id int unsigned not null,
+    java_identifications_on_a_figure_result_id int unsigned not null auto_increment,
+    usr_id int unsigned not null,
+    time_t int unsigned not null,
+    key inode_id (inode_id),
+    key java_identifications_on_a_figure_result_id (java_identifications_on_a_figure_result_id),
+    key usr_id (usr_id)
+) ;
+" ;
+
+$inicrond_db->Execute($query);
+
 ?>
